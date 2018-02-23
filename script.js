3c33338b649983c80d2ddbe873485daaa6679eaf
FreyaNoir <freyanoir@gmail.com>
2018-02-23 22:20:13 +0200

    Changes to fetch geolocation of the user and the current temperature from openweather API.

--- script.js
+++ script.js
@@ -1,73 +1,80 @@
 //changing random bgimg
-
+ 
 $(document).ready(function(){
-	var classCycle=['bg1','bg2','bg3','bg4','bg5','bg6','bg7','bg8',
-		'bg9','bg10','bg11','bg12','bg13','bg14','bg15','bg16','bg17'];
-
+    var classCycle=['bg1','bg2','bg3','bg4','bg5','bg6','bg7','bg8',
+        'bg9','bg10','bg11','bg12','bg13','bg14','bg15','bg16','bg17'];
+ 
     var randomNumber = Math.floor(Math.random() * classCycle.length);
     var classToAdd = classCycle[randomNumber];
-
+ 
 	$('.container-with-bgimg').addClass(classToAdd);
 	
-	var lat = "60.1699";
-	var lon = "24.9384";
-	var localTemp = getLocationForecast(lat, lon);
-	$('#localTemp').html(localTemp);
-	
-	
+	//Geolocation
+   
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+            makeRequest("/location/LAT/LON".replace('LAT', lat).replace('LON', lon), "POST", data => {
+                console.log("Server answered with data: ", data);
+                if (!data.error) {
+                    $('#city').val(data.location).change();
+                    $('#localTemp').html(data.temperature);
+                }
+            });
+        });
+    }
+   
 });
-
-
+ 
 const makeRequest = (url, method, onReady) => {
-	const xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function () {
-		if (this.readyState == 4 && this.status == 200) {
-			onReady(JSON.parse(xhttp.responseText));
-		}
-	};
-	console.log(url);
-	xhttp.open(method, url, true);
-	xhttp.send();
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            onReady(JSON.parse(xhttp.responseText));
+        }
+    };
+    console.log(url);
+    xhttp.open(method, url, true);
+    xhttp.send();
 };
-
+ 
 const updateObservations = () => {
-	makeRequest('/observations', 'GET', observations => {
-		const titleRow = {
-			location: 'City',
-			temperature: 'Temperature. (&#8451;)',
-			min: 'Min. temp. (&#8451;)',
-			max: 'Max. temp. (&#8451;)'
-		};
-		const obsTable = [titleRow].concat(observations).map(obs => {
-			const div = content => '<div>'+ (content ? content : '---')+'</div>';
-			return div(['location', 'temperature', 'min', 'max'].map(prop => obs[prop]).map(div).join(''));
-		}).join('');
-
-		document.getElementById('observations').innerHTML = obsTable;
-
-		if (document.getElementById('city').children.length === 0) {
-			const cityOptions = observations
-				.map(obs => '<option value="#">#</option>'.replace(/#/g, obs.location))
-				.join('');
-
-			document.getElementById('city').innerHTML = cityOptions;
-		}
-		clearTimeout(updateObservations);
-		setTimeout(updateObservations, 60 * 1000);
-	});
+    makeRequest('/observations', 'GET', observations => {
+        const titleRow = {
+            location: 'City',
+            temperature: 'Temperature. (&#8451;)',
+            min: 'Min. temp. (&#8451;)',
+            max: 'Max. temp. (&#8451;)'
+        };
+        const obsTable = [titleRow].concat(observations).map(obs => {
+            const div = content => '<div>'+ (content ? content : '---')+'</div>';
+            return div(['location', 'temperature', 'min', 'max'].map(prop => obs[prop]).map(div).join(''));
+        }).join('');
+ 
+        document.getElementById('observations').innerHTML = obsTable;
+ 
+        if (document.getElementById('city').children.length === 0) {
+            observations.forEach(obs => $('#city').append(
+                $('<option></option>').attr('value', obs.location).text(obs.location)
+            ));
+        }
+        clearTimeout(updateObservations);
+        setTimeout(updateObservations, 60 * 1000);
+    });
 };
-
+ 
 const submitData = () => {
-	const city = document.getElementById('city').value;
-	const temp = document.getElementById('temperature').value;
-	makeRequest('/observations/' + city + '/' + temp, 'POST', data => {
-		console.log(data);
-		if (data !== 'ok') {
-			alert('Error while submitting data: ' + data);
-		} else {
-			updateObservations();
-		}
-	});
+    const city = document.getElementById('city').value;
+    const temp = document.getElementById('temperature').value;
+    makeRequest('/observations/' + city + '/' + temp, 'POST', data => {
+        console.log(data);
+        if (data !== 'ok') {
+            alert('Error while submitting data: ' + data);
+        } else {
+            updateObservations();
+        }
+    });
 };
-
+ 
 updateObservations();
--- server.js
+++ server.js
@@ -1,132 +1,143 @@
 var serveStatic = require('serve-static');
 var connect = require('connect');
+const http = require('http');
 const weatherDb = require('./weather-db');
-var $ = require("jquery");
-
+ 
 let observations = [];
 weatherDb.connect();
 weatherDb.getAllData(data => observations = data);
-
+ 
 const port = process.env.PORT ? process.env.PORT : 3001;
+ 
+const locations = [
+    { city: 'Tokio', coordinates: { lat: 35.6584, lon: 139.7328 } },
+    { city: 'Dubai', coordinates: { lat: 25.0925, lon:  55.1562 } },
+    { city: 'Helsinki', coordinates: { lat: 60.1697, lon:  24.9490 } },
+    { city: 'Amsterdam', coordinates: { lat: 52.3650, lon:   4.9040 } },
+    { city: 'New York', coordinates: { lat: 40.7406, lon: -73.9938 } }
+];
+ 
+// This will fetch current temperature from openweathermap API according user coordinates
 
-/*
- API:
- GET	/observations
-	gets all latest observations and min and max values for all locations
- POST	/observations/{location}/{temperature}
-	adds a temperature observation for a location
-*/
-
-connect().use(serveStatic(__dirname)).use('/observations', (req, res) => {
-
-	const locations = ['Tokio', 'Dubai', 'Helsinki', 'Amsterdam', 'New York'];
-
-	const temperatureSanityCheck = temperature => 
-		!isNaN(temperature) && temperature > -70 && temperature < 65;
-
-	const validateAndAddObservation = (location, temperatureStr) => {
-		const temperature = Number(temperatureStr);
-		const errors = [
-			{ error: "City not found", success: locations.some(c => c === location) },
-			{ error: "Temperature invalid", success: temperatureSanityCheck(temperature) }
-		].filter(check => !check.success).map(err => err.error);
-		if (errors.length === 0) {
-			const observation = {
-				temperature,
-				location,
-				time: new Date()
-			};
-			observations.push(observation);
-			weatherDb.saveObservation(observation);
-		}
-		return errors;
-	};
+function getLocationForecast(lat, lon, onReady) {
+ 
+    var apiKey = '6129ebfe553acbb2aa2d83cf9b5eafb8';
+    var kelvin = 0;
+    var celcius = 0;
+ 
+    // Test if the location is near any of the places:
+	// (We use 0 decimals because 1 decimal is too fine)
 	
-	const calculateObservationsPerLocation = () => locations.map(location => {
-		const obsSortByTime = observations
-			.filter(obs => obs.location === location)
-			.sort((a, b) => b.time.getTime() - a.time.getTime());
-		const temp = value => value !== undefined ? value.toFixed(2) : undefined;
-		const temperature = temp(obsSortByTime[0] ? obsSortByTime[0].temperature : undefined);
-		const dayAgoMillis = new Date().getTime() - 24 * 3600 * 1000;
-		const temperatures = obsSortByTime
-			.filter(o => o.time.getTime() >= dayAgoMillis)
-			.map(o => o.temperature)
-			.sort((a, b) => a - b);
-		const min = temp(temperatures[0]);
-		const max = temp(temperatures[temperatures.length - 1]);
-		return { location, temperature, min, max };
-	});
-/*
-	// This will fetch current temperature from openweathermap API according to the user's coordinates.
-
-	const getLocationForecast = (lat, lon) => {
-
-		var apiKey = '6129ebfe553acbb2aa2d83cf9b5eafb8';
-		var kelvin = 0;
-		var celcius = 0;
-
-		var url="http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
-
-		$.getJSON(url, function(data) {
-			kelvin = parseFloat(data.main.temp);
-			celcius = parseFloat(kelvin - 273.15);
-		});
-
-		return celcius;
-	};
-	// Verifying if the user is on the correct location to store the temperature.
-
-	function check_city(lat, lon) {
-
-	var city = '';
-
-	// We use only 4 decimals that this exercise match better for located area. :)
-
-	lat = Math.trunc(lat * 10000) / 10000;
-	lon = Math.trunc(lon * 10000) / 10000;
-
-	// Allowed places for storing temperature values.
-
-	var places = {
-			'Tokio':      { lat: '35.6584', lon: '139.7328' },
-			'Helsinki':   { lat: '60.1697', lon:  '24.9490' },
-			'New York':   { lat: '40.7406', lon: '-73.9938' },
-			'Amsterdam':  { lat: '52.3650', lon:   '4.9040' },
-			'Dubai':      { lat: '25.0925', lon:  '55.1562' }
-	};
-
-	// This will find the city where the user is located.
-	
-	Object.keys(places).forEach(key => {
-	if (places[key].lat == lat && places[key].lon == lon) {
-		city = key;
-		// There could be break..
-	}
-	});
-	  
-	// This will return located city or false if user is out of specific cities.
-	if (city != "") {
-			return city;
-	} else {
-			return 'FALSE';
-	}
+    const place = locations.find(location => Math.abs(lat - location.coordinates.lat) < 1 && Math.abs(lon - location.coordinates.lon) < 1);
+ 
+    if (place === undefined) {
+        onReady();
+        return;
+    }
+ 
+    var url="http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+ 
+    requestUrl(url, function(data) {
+        console.log(data);
+        kelvin = parseFloat(data.main.temp);
+        celcius = parseFloat(kelvin - 273.15);
+        onReady(place.city, celcius);
+    });
 }
  
-// Test example
-lat = '40.74068888';
-lon = '-73.99389999';
-alert(check_city(lat, lon));
+const requestUrl = (url, callback) => {
+    http.get(url, res => {
+        res.setEncoding("utf8");
+        let body = "";
+        res.on("data", data => {
+          body += data;
+        });
+        res.on("end", () => {
+          body = JSON.parse(body);
+          callback(body);
+        });
+      });
+}
+ 
+const fileServer = serveStatic(__dirname);
+ 
+/*
+ API:
+ GET    /observations
+    gets all latest observations and min and max values for all locations
+ POST   /observations/{location}/{temperature}
+    adds a temperature observation for a location
+ POST   /location/{latitude}/{longitude}
+    returns temperature if your current location is near a supported location
 */
-
-	res.writeHead(200, { 'Content-Type': 'text/json' });
-	if (req.method === 'GET') {
-		res.end(JSON.stringify(calculateObservationsPerLocation()));
-	} else if (req.method === 'POST') {
-		const params = req.url.replace(/%20/g, ' ').replace('/', '').split('/');
-		const errors = validateAndAddObservation(params[0], params[1]);
-		res.end(JSON.stringify(errors.length === 0 ? 'ok' : errors));
-	}
-
-}).listen(port);
+connect().use((req, res, fn) => {
+    const serverFiles = [
+        'weather-db.js',
+        'server.js',
+        'package.json'
+    ];
+    if (serverFiles.some(f => req.url.indexOf(f) >= 0)) {
+        res.writeHead(404);
+        res.end();
+        return;
+    }
+    return fileServer(req, res, fn);
+}).use('/observations', (req, res) => {
+    const temperatureSanityCheck = temperature =>
+        !isNaN(temperature) && temperature > -70 && temperature < 65;
+ 
+    const validateAndAddObservation = (location, temperatureStr) => {
+        const temperature = Number(temperatureStr);
+        const errors = [
+            { error: "City not found", success: locations.some(c => c.city === location) },
+            { error: "Temperature invalid", success: temperatureSanityCheck(temperature) }
+        ].filter(check => !check.success).map(err => err.error);
+        if (errors.length === 0) {
+            const observation = {
+                temperature,
+                location,
+                time: new Date()
+            };
+            observations.push(observation);
+            weatherDb.saveObservation(observation);
+        }
+        return errors;
+    };
+   
+    const calculateObservationsPerLocation = () => locations.map(location => {
+        const obsSortByTime = observations
+            .filter(obs => obs.location === location.city)
+            .sort((a, b) => b.time.getTime() - a.time.getTime());
+        const temp = value => value !== undefined ? value.toFixed(2) : undefined;
+        const temperature = temp(obsSortByTime[0] ? obsSortByTime[0].temperature : undefined);
+        const dayAgoMillis = new Date().getTime() - 24 * 3600 * 1000;
+        const temperatures = obsSortByTime
+            .filter(o => o.time.getTime() >= dayAgoMillis)
+            .map(o => o.temperature)
+            .sort((a, b) => a - b);
+        const min = temp(temperatures[0]);
+        const max = temp(temperatures[temperatures.length - 1]);
+        return { location: location.city, temperature, min, max };
+    });
+   
+    res.writeHead(200, { 'Content-Type': 'text/json' });
+    if (req.method === 'GET') {
+        res.end(JSON.stringify(calculateObservationsPerLocation()));
+    } else if (req.method === 'POST') {
+        const params = req.url.replace(/%20/g, ' ').replace('/', '').split('/');
+        const errors = validateAndAddObservation(params[0], params[1]);
+        res.end(JSON.stringify(errors.length === 0 ? 'ok' : errors));
+    }
+})
+.use('/location', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/json' });
+    const params = req.url.replace('/', '').split('/');
+    const lat = Number(params[0]);
+    const lon = Number(params[1]);
+    getLocationForecast(lat, lon, (location, temperature) => {
+        const result = {error: location === undefined, location, temperature};
+        res.end(JSON.stringify(result));
+    });
+})
+.listen(port);
 console.log('Server running at http://localhost:' + port);
